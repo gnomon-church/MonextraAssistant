9207d17a8782df50a5ad27c26b86760dac62c394
Connor McHugh <connor.mchugh5@gmail.com>
2020-11-30 12:09:42 +1000

    Created pages for ISI management

---
+++ client/src/pages/SignOutISI.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class SignOutISI extends Component {
+    
+    render() {
+        return (
+            <h1>Sign Out ISI Books</h1>
+        )
+    }
+}
