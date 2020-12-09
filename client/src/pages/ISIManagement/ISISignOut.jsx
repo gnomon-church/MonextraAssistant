1709c9be544fc4a899d33a26e682106bb2d1dc81
Connor McHugh <connor.mchugh5@gmail.com>
2020-12-09 19:58:55 +1000

    ISI Management Page Structure

---
+++ client/src/pages/ISIManagement/ISISignOut.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class ISISignOut extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
