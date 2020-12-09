18d373f02b8dcfe4dce9adb1c1d271a74d3f298a
Connor McHugh <connor.mchugh5@gmail.com>
2020-12-09 15:36:56 +1000

    Navigation Component Improvement

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,11 +3,19 @@ import { Navbar, Button } from 'react-bootstrap';
 
 export default class Navigation extends Component {
     render() {
-        return (
-            <Navbar bg='danger' className='justify-content-between'>
-                <Button variant='dark' href='/'>Back</Button>
-                <Button variant='success' href={this.props.href}>Finish</Button>
-            </Navbar>
-        )
+        if (this.props.proceed === 'true') {
+            return (
+                <Navbar bg='danger' className='justify-content-between'>
+                    <Button variant='dark' href={this.props.from}>Back</Button>
+                    <Button variant='success' href={this.props.to}>Finish</Button>
+                </Navbar>
+            )
+        } else if (this.props.proceed === 'false') {
+            return (
+                <Navbar bg='danger' className='justify-content-between'>
+                    <Button variant='dark' href={this.props.from}>Back</Button>
+                </Navbar>
+            )
+        }
     }
 }
