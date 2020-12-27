5bbc1b3cd650aa6680c53a817a206826bc8f2667
Connor McHugh <connor.mchugh5@gmail.com>
2020-12-27 22:47:40 +1000

    Receive date & ID logic
    Implemented date selector for shipment receiving modal.
    Shipment ID input will now only accept numeric data

--- client/src/pages/ISIManagement/ISIReceive.jsx
+++ client/src/pages/ISIManagement/ISIReceive.jsx
@@ -4,16 +4,21 @@ import { Modal, Button, InputGroup, FormControl } from 'react-bootstrap';
 // import { AgGridReact } from 'ag-grid-react';
 // import axios from 'axios'
 import DatePicker from "react-datepicker";
- 
+
 import "react-datepicker/dist/react-datepicker.css";
 
 import Navigation from '../../components/Navigation'
+
+let shipment_data = {
+    shipment_id: '',
+    date_received: '',
+}
 
 export default function ISIReceive() {
     let history = useHistory()
 
     const [showShipmentIdDialog, setShowShipmentIdDialog] = useState(true);
-    const [startDate, setStartDate] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
 
     const closeShipmentIdDialog = () => setShowShipmentIdDialog(false);
     const openShipmentIdDialog = () => setShowShipmentIdDialog(true);
@@ -21,6 +26,19 @@ export default function ISIReceive() {
     useEffect(() => {
         document.title = 'Receive ISI - Mona';
     }, []);
+
+    // Ensure that only numbers are entered in corresponding input boxes
+    function numberValidator(event) {
+        let re = /^[0-9]*$/;
+        let val = re.exec(event.target.value);
+
+        if (val !== null) {
+            event.target.value = val[0]
+            shipment_data[event.target.name] = event.target.value
+        } else {
+            event.target.value = shipment_data[event.target.name]
+        }
+    }
 
     return (
         <div>
@@ -40,12 +58,32 @@ export default function ISIReceive() {
                 <Modal.Body>
                     <InputGroup className='mb-3'>
                         <InputGroup.Prepend>
+                            <InputGroup.Text>Received Date</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <DatePicker
+                            className='form-control'
+                            selected={selectedDate}
+                            dateFormat='dd/MM/yyyy'
+                            onChange={(date) => (setSelectedDate(date))}
+                        // customInput={
+                        //     <div>
+                        //     <FormControl type='text'
+                        //         value={selectedDate}
+                        //         name='date'
+                        //     />
+                        //     </div>
+                        // } 
+                        />
+                    </InputGroup>
+
+                    <InputGroup className='mb-3'>
+                        <InputGroup.Prepend>
                             <InputGroup.Text>Shipment ID</InputGroup.Text>
                         </InputGroup.Prepend>
                         <FormControl
                             type='text'
-                            // onChange={}
-                            name='ticket_name'
+                            onChange={numberValidator}
+                            name='shipment_id'
                         />
                     </InputGroup>
                 </Modal.Body>
