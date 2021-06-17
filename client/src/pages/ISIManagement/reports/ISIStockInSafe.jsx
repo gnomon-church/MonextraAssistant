36043915b80f92ac5ae694b941774690a3da6554
Connor McHugh <connor.mchugh5@gmail.com>
2021-06-17 11:08:17 +1000

    Pre object to array conversion

--- client/src/pages/ISIManagement/reports/ISIStockInSafe.jsx
+++ client/src/pages/ISIManagement/reports/ISIStockInSafe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Navbar, ButtonGroup } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
@@ -6,10 +6,13 @@ import axios from 'axios';
 export default function PDFReport() {
 	let history = useHistory();
 
-	let idReg = /^[0-9]{4}/;
-	let bookReg = /^[0-9]{4}([0-9]{6})/;
+	const [safeBooks, setSafeBooks] = useState([]);
+	const [bookDetails, setBookDetails] = useState([]);
 
-	setBookData(book);
+	useEffect(() => {
+		document.title = 'Stock in Safe - Mona';
+		fetchData();
+	}, []);
 
 	// Get data from the API and set it
 	function fetchData() {
@@ -24,8 +27,88 @@ export default function PDFReport() {
 					};
 				})
 			)
-			.then((book) => {});
+			.then((books) => {
+				let bookObject = {};
+				for (let i = 0; i < books.length; i++) {
+					if (books[i].game_id in bookObject) {
+						bookObject[books[i].game_id].push(books[i].book_number);
+					} else {
+						bookObject[books[i].game_id] = [books[i].book_number];
+					}
+				}
+				let bookArray = [];
+				for (const [key, value] of Object.entries(bookObject)) {
+					bookArray.push([key, value]);
+				}
+				setSafeBooks(bookArray);
+			});
 	}
+
+	const DisplayBooks = () => {
+		for (let i = 0; i < safeBooks.length; i++) {
+			axios
+				.get('/api/isi-game-details/' + safeBooks[i][0])
+				.then((res) => res.data.rows)
+				.then((rows) =>
+					rows.map((book) => {
+						return {
+							ticket_value: book.ticket_value,
+							ticket_name: book.ticket_name,
+						};
+					})
+				)
+				.then((details) => {
+					// let detailArray = bookDetails;
+					// detailArray.push(details[0]);
+					// setBookDetails(detailArray);
+					setBookDetails(bookDetails.concat(details[0]));
+				});
+		}
+
+		console.log(safeBooks);
+		console.log(bookDetails);
+
+		return null;
+
+		// return Object.keys(safeBooks).map((key) => {
+		// 	axios
+		// 		.get('/api/isi-game-details/' + key)
+		// 		.then((res) => res.data.rows)
+		// 		.then((rows) =>
+		// 			rows.map((book) => {
+		// 				return {
+		// 					ticket_value: book.ticket_value,
+		// 					ticket_name: book.ticket_name,
+		// 				};
+		// 			})
+		// 		)
+		// 		.then((details) => setBookDetails(details));
+
+		// 	console.log(bookDetails);
+
+		// 	return (
+		// 		<tbody>
+		// 			<tr>
+		// 				{/* <th>{bookDetails[0].ticket_name}</th> */}
+		// 				<th>{safeBooks[key].length}</th>
+		// 			</tr>
+
+		// 			<tr>
+		// 				<td>{key}</td>
+		// 				<td>{safeBooks[key][0]}</td>
+		// 			</tr>
+		// 			<tr>
+		// 				<td>{key}</td>
+		// 				<td>{safeBooks[key][1]}</td>
+		// 			</tr>
+		// 			<tr>
+		// 				<td>{key}</td>
+		// 				<td>{safeBooks[key][2]}</td>
+		// 			</tr>
+		// 		</tbody>
+		// 	);
+		// });
+	};
 
 	return (
 		<div>
@@ -43,7 +126,9 @@ export default function PDFReport() {
 				</ButtonGroup>
 			</Navbar>
 
-			<div className='print-content'></div>
+			<div className='print-content'>
+				<DisplayBooks />
+			</div>
 		</div>
 	);
 }
